ea1c9eaa1e6fd6963db469c70bd9294ee4a8fde4
DESKTOP-2A54VHJ\user <jackal132@naver.com>
2021-02-26 14:52:39 +0900

    State 사용과 Key

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,12 +2,18 @@ import { Component } from 'react';
 
 class TOC extends Component{
     render(){
+      var list = [];
+      var data = this.props.data;
+      var i = 0;
+      while(i < data.length){
+          // 여러 목록을 자동으로 생성할때는 key prop을 가지고 있어야 한다.
+          list.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+          i = i + 1;
+      }
       return (
         <nav>
             <ul>
-                <li><a href="1.html">HTML</a></li>
-                <li><a href="2.html">CSS</a></li>
-                <li><a href="3.html">JavaScript</a></li>
+                {list}
             </ul>
         </nav>
       );
