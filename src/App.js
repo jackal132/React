7d28fe9daa6cfc37522b92075c91da453125de2e
DESKTOP-2A54VHJ\user <jackal132@naver.com>
2021-02-26 14:41:29 +0900

    State 사용

--- src/App.js
+++ src/App.js
@@ -6,10 +6,22 @@ import Subject from './components/Subject';
 
 
 class App extends Component{
+  // 컴포넌트가 실행될때 랜더함수보다 먼저 실행되면서
+  // 콤포넌트를 초기화시켜주고 싶은 코드는 constructor 안에 작성한다.
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title : 'WEB', sub:'World Wide Web'}
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="World wide web!"></Subject>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>        
         <TOC></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
