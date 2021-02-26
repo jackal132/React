1262d206558f0c4d1f69f2360ef826d89c0a4272
DESKTOP-2A54VHJ\user <jackal132@naver.com>
2021-02-26 15:33:48 +0900

    컴포넌트 이벤트 만들기

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -5,7 +5,10 @@ class Subject extends Component{
       return (
         //컴포넌트를 만들때는 반드시 하나의 최상위 태그만 있어야한다.
         <header>
-            <h1><a href="/">{this.props.title}</a></h1>
+            <h1><a href="/" onClick={function(e){
+                e.preventDefault();
+                this.props.onChangePage();
+            }.bind(this)}>{this.props.title}</a></h1>
             {this.props.sub}
         </header>
       );
